34a538c1b2227d65c3988965bc84700982b53a5d
mdekabs <1mawoda@gmail.com>
2022-12-20 17:39:43 +0100

    all

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" object oriented programming in python """
+
+
+class Square():
+    """Class of square"""
+
+    def __init__(self, size):
+        """ initialize class
+        Args:
+           size (int): size of the square
+        """
+        self.__size = size
